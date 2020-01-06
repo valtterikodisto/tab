458c505dfa7cd40996d23ccde2e530c545f1f80e
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2020-01-07 01:04:25 +0200

    Fixed restore script

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -4,12 +4,16 @@ TARGET="/media/$USER/TAB/backup"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESETCOLOR="\033[0m"
+PROJECT_ROOT="/opt/tab"
+
+restart_app() {
+    sudo docker-compose -f $PROJECT_ROOT/docker-compose.yml restart
+}
 
 # Start the application if not running
 IS_RUNNING=$(docker inspect -f '{{.State.Running}}' tab-db)
 if [ "$IS_RUNNING" != "true" ]; then
-  #docker-compose -f /opt/tab/docker-compose.yml up -d
-  docker-compose -f /home/valtteri/Courses/fullstack/tab/docker-compose.yml up -d
+  docker-compose -f $PROJECT_ROOT/docker-compose.yml up -d
 fi
 
 while :
@@ -21,9 +25,8 @@ do
   if [ -f "$TARGET/$backup" ]; then
     docker exec -i tab-db sh -c 'exec mongorestore --drop --batchSize=1 --archive ' < "$TARGET/$backup" && printf "\n${GREEN}Tiedoston palautus onnistui!\n${RESETCOLOR}"
 
-    # Restart
-    docker-compose -f /home/valtteri/Courses/fullstack/tab/docker-compose.yml down
-    docker-compose -f /home/valtteri/Courses/fullstack/tab/docker-compose.yml up -d
+    restart_app
+
     break
   else
     printf "${RED}Palautuskansiota ${TARGET}/${backup} ei löytynyt${RESETCOLOR}\n\n"
