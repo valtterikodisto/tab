63ca6e000dd30a17e9e2bf8d24925174315879a6
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2020-01-07 00:36:18 +0200

    App restarts when updated

--- scripts/update.sh
+++ scripts/update.sh
@@ -14,12 +14,16 @@ prompt_exit() {
   read -r -p "Paina ENTER poistuaksesi" _
 }
 
+restart_app() {
+    sudo docker-compose -f $PROJECT_ROOT/docker-compose.yml restart
+}
+
 if [ $LOCAL != $REMOTE ]; then
     echo "Päivitys saatavilla!"
     read -r -p "Haluatko päivittää sovelluksen? [y/n] " response
     
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        sudo mv /opt/tab/ /opt/tab-old/ && cd /opt/tab-old/scripts/ && ./install.sh && sudo mv /opt/tab-old/data/ /opt/tab/ && sudo rm -rf /opt/tab-old/ && printf "${GREEN}Sovellus päivitetty!\n\n${RESETCOLOR}" && prompt_exit
+        sudo mv /opt/tab/ /opt/tab-old/ && cd /opt/tab-old/scripts/ && ./install.sh && sudo mv /opt/tab-old/data/ /opt/tab/ && sudo rm -rf /opt/tab-old/ && restart_app && printf "${GREEN}Sovellus päivitetty!\n\n${RESETCOLOR}" && prompt_exit
     else
         echo "Päivitys peruutettu"
         prompt_exit
